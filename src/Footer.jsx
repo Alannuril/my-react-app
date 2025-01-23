e67d808e67eac26c8700ee150efc7fa862a0f0c5
Alannuril <alannuril013@gmail.com>
2025-01-24 00:02:43 +0700

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    return (
+        <footer>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dicta perspiciatis illum mollitia modi at necessitatibus temporibus explicabo voluptatibus dolorum repellendus eveniet dignissimos accusantium earum repellat nisi, neque officia quo.</p>
+            <p>&copy; {new Date().getFullYear()} your website name</p>
+        </footer>
+    );
+}
+
+export default Footer
